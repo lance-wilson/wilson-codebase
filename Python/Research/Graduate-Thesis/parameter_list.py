d71ee0c4ae84e28034c440ba08cbc93e541f9c47
lance-wilson <89750654+lance-wilson@users.noreply.github.com>
2021-12-18 15:41:38 -0600

    Add files via upload

---
+++ Python/Research/Graduate-Thesis/parameter_list.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+#
+# Name:
+#   parameter_list.py
+#
+# Purpose:  Store the values of the plotting parameter dictionary separately so
+#           that it doesn't have to take space at the beginning of the plotting
+#           files. Also includes dictionaries of colorbar labels and matplotlib
+#           legend labels for vorticity budget variables, and a dictionary for
+#           substituting the dimension of the data.
+#
+# Syntax: from parameter_list import parameters
+#         parameters = parameters(variable)
+#
+# Execution Example:
+#   from parameter_list import parameters
+#   parameters = parameters('dbz')
+#
+# Modification History:
+#   2021/07/28 - Lance Wilson:  Created.
+#
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Parameters used for plotting trajectory data (both back and forward).
+#   val_interval:       Distance between tick labels on the colorbar.
+#   datamin:            Minimum value variable on the colorbar.
+#   datamax:            Maximum value variable on the colorbar.
+#   offset:             Vertical height of the layer (in CM1 grid points).
+#   contour_interval:   Number of contours to plot.
+#   colormap:           Matplotlib colormap to use.
+#   bar_label:          Label for the color bar.
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+def parameters(variable):
+    parameters = {  'dbz': {'val_interval'      : 5,
+                        'datamin'           : -35,
+                        'datamax'           : 65,
+                        'offset'            : 0,
+                        'contour_interval'  : 100,
+                        'colormap'          : 'gist_ncar',
+                        'bar_label'         : 'Reflectivity (dbz)'},
+              'xvort': {'val_interval'      : 0.1,
+                        'datamin'           : -0.35,
+                        'datamax'           : 0.55,
+                        'offset'            : 10,
+                        'contour_interval'  : 10,
+                        'colormap'          : 'PuOr',
+                        'bar_label'         : 'Horizontal Vorticity (1/s)'},
+             'thpert': {'val_interval'      : 2,
+                        'datamin'           : -5.0,
+                        'datamax'           : 5.0,
+                        'offset'            : 5,
+                        'contour_interval'  : 11,
+                        'colormap'          : 'PuOr',
+                        'bar_label' : 'Potential Temperature Perturbation (K)'},
+                  'u': {'val_interval'      : 12,
+                        'datamin'           : -30.0,
+                        'datamax'           : 30.0,
+                        'offset'            : 25,
+                        'contour_interval'  : 11,
+                        'colormap'          : 'PuOr',
+                        'bar_label'         : 'E-W Horizontal Velocity (m/s)'},
+                  'v': {'val_interval'      : 12,
+                        'datamin'           : -30.0,
+                        'datamax'           : 30.0,
+                        'offset'            : 25,
+                        'contour_interval'  : 11,
+                        'colormap'          : 'PuOr',
+                        'bar_label'         : 'N-S Horizontal Velocity (m/s)'},
+                  'w': {'val_interval'      : 2,
+                        'datamin'           : -5.0,
+                        'datamax'           : 5.0,
+                        'offset'            : 25,
+                        'contour_interval'  : 11,
+                        'colormap'          : 'PuOr',
+                        'bar_label'         : 'Vertical Velocity (m/s)'}
+             }
+    return parameters[variable]
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Labels to be used for the vorticity budget variable colorbar (when plotting
+#   trajectory paths spatially).
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+def budget_barlabels(variable):
+
+    bar_labels = {
+            'x_stretch_term'   : r'E-W Stretching (s$\mathregular{^{-2}}$)',
+            'y_stretch_term'   : r'N-S Stretching (s$\mathregular{^{-2}}$)',
+            'z_stretch_term'   : r'Vertical Stretching (s$\mathregular{^{-2}}$)',
+            'x_tilt_term'      : r'E-W Tilting (s$\mathregular{^{-2}}$)',
+            'y_tilt_term'      : r'N-S Tilting (s$\mathregular{^{-2}}$)',
+            'z_tilt_term'      : r'Vertical Tilting (s$\mathregular{^{-2}}$)',
+            'x_solenoid_term'  : r'E-W Solenoid (s$\mathregular{^{-2}}$)',
+            'y_solenoid_term'  : r'N-S Solenoid (s$\mathregular{^{-2}}$)',
+            'z_solenoid_term'  : r'Vertical Solenoid (s$\mathregular{^{-2}}$)',
+            'x_advection_term' : r'E-W Advection (s$\mathregular{^{-2}}$)',
+            'y_advection_term' : r'N-S Advection (s$\mathregular{^{-2}}$)',
+            'z_advection_term' : r'Vertical Advection (s$\mathregular{^{-2}}$)',
+            'xvortb_buoy'      : r'E-W Vort. Buoyancy (s$\mathregular{^{-2}}$)',
+            'yvortb_buoy'      : r'N-S Vort. Buoyancy (s$\mathregular{^{-2}}$)',
+            'xvortb_hadv'      : r'Horiz. Advection of E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_hadv'      : r'Horiz. Advection of N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_hadv'      : r'Horiz. Advection of Vertical Vort. (s$\mathregular{^{-2}}$)',
+            'xvortb_vadv'      : r'Vert. Advection of E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_vadv'      : r'Vert. Advection of N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_vadv'      : r'Vert. Advection of Vertical Vort. (s$\mathregular{^{-2}}$)',
+            'xvortb_hedif'     : r'Horiz. Diffusion of E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_hedif'     : r'Horiz. Diffusion of N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_hedif'     : r'Horiz. Diffusion of Vertical Vort. (s$\mathregular{^{-2}}$)',
+            'xvortb_vedif'     : r'Vert. Diffusion of E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_vedif'     : r'Vert. Diffusion of N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_vedif'     : r'Vert. Diffusion of Vertical Vort. (s$\mathregular{^{-2}}$)',
+            'xvortb_hturb'     : r'Horiz. Turbulence, E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_hturb'     : r'Horiz. Turbulence, N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_hturb'     : r'Horiz. Turbulence, Vertical Vort. (s$\mathregular{^{-2}}$)',
+            'xvortb_vturb'     : r'Vert. Turbulence, E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_vturb'     : r'Vert. Turbulence, N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_vturb'     : r'Vert. Turbulence, Vertical Vort. (s$\mathregular{^{-2}}$)',
+            'xvortb_pgrad'     : r'Pressure Gradient, E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_pgrad'     : r'Pressure Gradient, N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_pgrad'     : r'Pressure Gradient, Vertical Vort. (s$\mathregular{^{-2}}$)',
+            # Combined terms
+            'xvortb_turb'      : r'Total Turbulence, E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_turb'      : r'Total Turbulence, N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_turb'      : r'Total Turbulence, Vertical Vort. (s$\mathregular{^{-2}}$)',
+            'xvortb_edif'      : r'Total Diffusion of E-W Vort. (s$\mathregular{^{-2}}$)',
+            'yvortb_edif'      : r'Total Diffusion of N-S Vort. (s$\mathregular{^{-2}}$)',
+            'zvortb_edif'      : r'Total Diffusion of Vertical Vort. (s$\mathregular{^{-2}}$)',
+        }
+    return bar_labels[variable]
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Labels to be used for the vorticity budget variable legend labels (when
+#   plotting trajectory components vs. time).
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+def budget_legendlabels(variable):
+
+    legend_labels = {
+                '_stretch_term'   : 'Stretching',
+                '_tilt_term'      : 'Tilting',
+                '_solenoid_term'  : 'Solenoid',
+                '_advection_term' : 'Advection',
+                'vortb_buoy'      : 'Buoyancy',
+                'vortb_hadv'      : 'Horiz. Advection',
+                'vortb_vadv'      : 'Vert. Advection',
+                'vortb_hedif'     : 'Hor. Diffusion',
+                'vortb_vedif'     : 'Vert. Diffusion',
+                'vortb_hturb'     : 'Horiz. Turbulence',
+                'vortb_vturb'     : 'Vert. Turbulence',
+                'vortb_pgrad'     : 'Pressure Gradient',
+                # Combined terms
+                'vortb_turb'      : 'Total Turbulence',
+                'vortb_edif'      : 'Total Diffusion',
+            }
+    return legend_labels[variable[1:]]
+
+def title_dir_sub(variable):
+    title_dir_sub = {
+                'x' : 'East-West Component of',
+                'y' : 'North-South Component of',
+                'z' : 'Vertical Component of',
+                }
+    return title_dir_sub[variable]
+
---
+++ Python/Research/Graduate-Thesis/plot_back_traj_vort_component.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+#
+# Name:
+#   plot_back_traj_vort_component.py
+#
+# Purpose:  Plot CM1 backward trajectories visualizing vorticity budget data
+#           along the trajectories.
+#
+# Syntax: python3 plot_back_traj_vort_component.py version_number parcel_label
+#
+#   Input:
+#
+# Execution Example:
+#   python3 plot_back_traj_vort_component.py v5 downdraft
+#
+# Modification History:
+#   2019/09/19 - Lance Wilson:  Modified from code written by Tom Gowan, using
+#                               trajectories_plot.ipynb from:
+# https://github.com/tomgowan/trajectories/blob/master/trajectories_plot.ipynb
+#   2019/10/01 - Lance Wilson:  Split 3D plot into separate file, as that is
+#                               likely what will be used going forward.
+#   2020/11/23 - Lance Wilson:  New file to plot trajectory positions from a
+#                               cm1out_pdata.nc file.
+#   2020/12/18 - Lance Wilson:  Attempting to combine
+#                               plot_trajectory_3d_20191001.py and
+#                               plot_output_trajectory_3d_test.py.
+#   2021/02/23 - Lance Wilson:  Plot script to go with 1D array version of
+#                               calc_back_trajectory_meters.
+#   2021/09/17 - Lance Wilson:  Simplified for loop in plotting section.
+#   2021/10/26 - Lance Wilson:  Split from plot_back_traj_simpler_loop to
+#                               accommodate vorticity budget data.
+#   2021/11/09 - Lance Wilson:  Modified to calculate the file numbers to plot
+#                               before the plotting loop (eliminates the need
+#                               for the time loop).
+#   2021/11/18 - Lance Wilson:  Split from plot_back_traj_budgets to begin
+#                               plotting graphs of each component for an
+#                               individual trajectory vs. time.
+#
+
+from back_traj_interp_class import Back_traj_ds
+from parameter_list import budget_legendlabels, title_dir_sub
+
+from netCDF4 import Dataset
+from netCDF4 import MFDataset
+
+import matplotlib
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+mandatory_arg_num = 2
+
+if len(sys.argv) > mandatory_arg_num:
+    version_number = sys.argv[1]
+    parcel_label = sys.argv[2]
+else:
+    print('Parcel label and version number must be specified.')
+    print('Syntax: python3 plot_back_traj_vort_component.py version_number parcel_label')
+    print('Example: python3 plot_back_traj_vort_component.py v5 downdraft')
+    print('Currently supported version numbers: v4, v5')
+    print('Set budget_variable to \'all\' to plot all variables.')
+    sys.exit()
+
+if version_number == '3' or version_number =='10s':
+    print('Version number is not valid.')
+    print('Currently supported version numbers: 4, 5')
+    sys.exit()
+
+if version_number.startswith('v'):
+    run_number = int(version_number[-1])
+else:
+    run_number = 3
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# User-Defined Input/Output Directories and Constants
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Directory with CM1 model output.
+model_dir = '/vortex2/lwilson/75m_100p_{:s}/'.format(version_number)
+# Directory where back trajectory analysis data is stored
+analysis_dir = model_dir + 'back_traj_analysis/'
+# Directory with netCDF files of vorticity budget data interpolated to trajectory locations.
+interp_dir = analysis_dir + 'parcel_interpolation/'
+# Directory for output images.
+output_dir = analysis_dir + 'BackTrajectoryImages/'
+
+# How far back (in minutes) to plot trajectories.
+plot_limit_minutes = 10.
+
+# Trajectory to be plotted.
+# For now, this is a constant, but will hopefully be calculated in another program at some point.
+plot_index = 100
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Open object using netCDF files containing vorticity budget data interpolated
+#   to back trajectory locations.
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ds_obj = Back_traj_ds(version_number, interp_dir, parcel_label)
+
+# Initialization Position time for parcels.
+initialize_time = int(ds_obj.simulation_times[0])
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Create a dictionary of the six subsets of vorticity budget data that are to
+#   be plotted (representing both the direct vorticity equation calculation and
+#   the vorticity tendencies calculated from CM1 momentum budget in all three
+#   spatial dimensions). 
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+budget_var_names = {}
+for axis in ['x', 'y', 'z']:
+    budget_var_names[axis + '_budget'] = []
+    budget_var_names[axis + '_equation'] = []
+# Sort the budget variable labels into the appropriate subset of the dictionary.
+for key in ds_obj.budget_var_keys:
+    # Since the trajectories are moving along with the flow, need to look at
+    #   the total derivative, so don't include the advection terms.
+    if 'advection' in key or 'adv' in key:
+        pass
+    # Since the model tendency advection terms include the components for
+    #   stretching and tilting, the manually calculated components must be
+    #   included in the model budget terms.
+    elif 'stretch' in key or 'tilt' in key:
+        budget_var_names[key[0] + '_budget'].append(key)
+        budget_var_names[key[0] + '_equation'].append(key)
+    else:
+        if 'vortb' in key:
+            # Turbulence and diffusion variables will be defined; vertical
+            #   component is skipped so that they are not added in twice.
+            if 'vturb' in key or 'vedif' in key:
+                pass
+            # Add a combined form of the turbulence and diffusion variables to
+            #   list of budget variables.
+            elif 'hturb' in key or 'hedif' in key:
+                budget_var_names[key[0] + '_budget'].append(ds_obj.remove_dirFromVar(key))
+            else:
+                budget_var_names[key[0] + '_budget'].append(key)
+        else:
+            budget_var_names[key[0] + '_equation'].append(key)
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Calculate how many time steps of trajectory data to plot (based on
+#   user-defined number of minutes to be plotted).
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Convert plot_limit_minutes to seconds.
+plot_limit_seconds = 60. * plot_limit_minutes
+
+# Array of all time steps lengths for this set of trajectory data.
+time_step_lengths = np.round(ds_obj.simulation_times[:-1] - ds_obj.simulation_times[1:])
+
+# Cumulative sum array of the time step lengths, which are already oriented
+#   backward in time, and thus can tell how many indices to count backward
+#   directly.
+cumulative_time_steps = np.cumsum(time_step_lengths)
+
+# Check that at least one time step is going to be plotted (i.e.
+#   plot_limit_seconds must be no smaller than the smallest time_step),
+#   otherwise np.argwhere result will be empty and crash the program.
+if plot_limit_seconds < time_step_lengths[-1]:
+    print('Length of time desired to plot is less than the time step, ' + 
+          'so there is no trajectory data to plot.')
+    sys.exit()
+
+# How far back (in time steps) to plot trajectories.
+#   The final row of the np.argwhere output contains the index of the last
+#   appropriate time, so the limit for the plotting loop is one more than that.
+plot_limit = np.argwhere(cumulative_time_steps <= plot_limit_seconds)[-1,0] + 1
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+#                                                                    #
+#   Plot Budget Components                                           #
+#                                                                    #
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+for budget_coord_type, budget_var_list in budget_var_names.items():
+    # Initialize the plot.
+    fig2 = plt.figure(figsize=(13,7))
+    ax = fig2.add_subplot(111)
+
+    budget_vars_sum = np.zeros((ds_obj.parcel_time_step_num))
+
+    for budget_var_name in budget_var_list:
+        # For the turbulence and diffusion variables, add them together since
+        #   they are small and the difference between the horizontal and
+        #   vertical components is not important.
+        if budget_var_name.endswith('turb') or budget_var_name.endswith('edif'):
+            budget_var = ds_obj.getCombinedBudgetData(budget_var_name)[:,plot_index]
+        # Other tendencies are large enough to be treated separately.
+        else:
+            budget_var = ds_obj.getBudgetData(budget_var_name)[:,plot_index]
+        # Sum of the vorticity tendencies.
+        budget_vars_sum += budget_var
+
+        # Plot of individual vorticity tendencies.
+        plt.plot(ds_obj.simulation_times[:plot_limit][::-1], budget_var[:plot_limit][::-1], label=budget_legendlabels(budget_var_name))
+
+    # Plot of sum of the vorticity tendencies.
+    plt.plot(ds_obj.simulation_times[:plot_limit][::-1], budget_vars_sum[:plot_limit][::-1], label='Total Vort.', color='black')
+
+    # Reference line at 0.
+    plt.plot(ds_obj.simulation_times[:plot_limit][::-1], np.zeros(ds_obj.simulation_times[:plot_limit].shape), color='black')
+
+    # Title string for whether this is the model-based budget calculation or
+    #   the direct equation calculation.
+    equation_string = budget_coord_type.split('_')[-1].title()
+    if equation_string == 'Budget':
+        context_string = ' from CM1 Output'
+    else:
+        context_string = ', Directly Calculated'
+    # Dimension of the data to put in the title.
+    direction = budget_coord_type.split('_')[0]
+    dir_string = title_dir_sub(direction)
+
+    plt.title('{:s} Vorticity {:s} Components{:s}'.format(dir_string, equation_string, context_string))
+    plt.xlim(ds_obj.simulation_times[plot_limit-1], initialize_time)
+    plt.xlabel('Simulation Time (seconds)')
+    plt.ylabel(r'Vorticity Tendency (s$\mathregular{^{-2}}$)')
+    plt.legend(loc='upper left')
+
+    #image_file_name = output_dir + 'cm1_backtraj_vortbudget_components_{:s}_{:s}_inittime{:d}_p{:d}.png'.format(parcel_label, budget_coord_type, initialize_time, plot_index)
+    #plt.savefig(image_file_name, dpi=400)
+
+    plt.show()
+
